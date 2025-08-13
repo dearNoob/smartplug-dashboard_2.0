690ffe507269e66325506537b139395b74a8c95c
Mehedi Hasan <mhshakib96@gmail.com>
2025-08-14 05:57:53 +0600

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Database configuration (Please ensure this is correct)
+// Read database configuration from Environment Variables
 const dbConfig = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: '1234',
-  database: 'tuya_db',
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT || 3306
 };
 
 // Middleware
